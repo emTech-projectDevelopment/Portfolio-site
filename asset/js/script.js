c7073092ede95a5d3d3bb84f9144efeeb02f49f3
emTech-projectDevelopment <emtech.projectdevelopment@gmail.com>
2022-02-28 07:49:06 +1100

    initial commit

---
+++ asset/js/script.js
@@ -0,0 +1,35 @@
+console.log('Script Ready')
+
+const screenshots = './asset/images/Project Screenshots/'
+
+const projectItems = [
+    {title: "JS Calculator", projectDescription : "Calculator App using HTML, CSS and Javascript", repo: "https://github.com/emTech-projectDevelopment/Calculator-Project", live: "https://emtech-projectdevelopment.github.io/Calculator-Project/", imgSrc: "./asset/images/Project-Screenshots/CALCULATOR-JS.png", imgAlt: "JS Calculator"},
+    {title: "CSS Animation Demo", projectDescription : "Animation demonstration using HTML and CSS", repo: "https://github.com/emTech-projectDevelopment/Fantastic-Devs-Css-Demo", live: "https://emtech-projectdevelopment.github.io/Fantastic-Devs-Css-Demo/", imgSrc: "./asset/images/Project-Screenshots/CSS-DEMO.png", imgAlt: "css demo"},
+    {title: "JS Quiz Challenge", projectDescription : "Generation AU | AcademyXi quiz challenge using HTML, CSS and Javascript", repo: "https://github.com/emTech-projectDevelopment/octo-lamp-five", live: "https://emtech-projectdevelopment.github.io/octo-lamp-five/", imgSrc: "./asset/images/Project-Screenshots/QUIZ.png", imgAlt: "quiz app"},
+    {title: "JS Clock", projectDescription : "Clock App using HTML, CSS and Javascript", repo: "https://github.com/emTech-projectDevelopment/Clock-Project", live: "https://emtech-projectdevelopment.github.io/Clock-Project/", imgSrc: "./asset/images/Project-Screenshots/CLOCK-JS.png", imgAlt: "clock app"},
+    {title: "Shopping Cart", projectDescription : "Generation AU | AcademyXi Shopping cart challenge using HTML, CSS and Javascript", repo: "https://emtech-projectdevelopment.github.io/Shopping-Cart-Challenge/", live: "https://github.com/emTech-projectDevelopment/Shopping-Cart-Challenge", imgSrc: "./asset/images/Project-Screenshots/SHOPPING-CART.png", imgAlt: "shopping cart js"},
+    {title: "To Do App", projectDescription : "Generation AU | AcademyXi To Do App - Assesment - Using HTML, CSS and Javascript", repo: "https://github.com/emTech-projectDevelopment/je-final-project", live: "https://gollanj.github.io/je-final-project/", imgSrc: "./asset/images/Project-Screenshots/TO-DO_APP.png", imgAlt: "to do app"},
+    {title: "Vintage Car Rental Website", projectDescription : "Generation AU | AcademyXi Responsive web site challenge using HTML and CSS", repo: "https://github.com/emTech-projectDevelopment/ColmarWebAssessement", live: "https://emtech-projectdevelopment.github.io/ColmarWebAssessement/", imgSrc: "./asset/images/Project-Screenshots/VINTAGE-CAR-RENTAL.png", imgAlt: "vintage car website"}
+];
+
+
+function populateprojects(data){
+    const projectsWrapper = document.querySelector('.projects-wrapper');
+
+    for (let i=0; i<projectItems.length;i++){
+       let projectItemsHtml = 
+        `<div class="project-item">
+                <h4 class="project-title">${data[i].title}</h4>
+                <a href=${data[i].live}>
+                    <img class="project-image" src=${data[i].imgSrc} alt=${data[i].imgAlt}/>
+                <a/>
+                <p class="project-description">${data[i].projectDescription}</p>
+                <a class="project-link" href=${data[i].repo}>${data[i].title} Github Repository</a>
+                <a class="project-link" href=${data[i].live}>${data[i].title} Github Live Site</a>
+         </div>`;
+
+        projectsWrapper.innerHTML +=  projectItemsHtml;
+    }
+}  
+ 
+populateprojects(projectItems)
